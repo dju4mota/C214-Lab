873cd1ea1fb5deecfa59ccc99161cb92e6eea410
dju4mota <fernandomotajunior2@gmail.com>
2024-03-16 20:21:24 -0300

    adicionado jest e testes

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -18,9 +18,20 @@ export class ToDoList{
 
     private tasks: Task[] =[]
     
-    add(task: Task){
-        this.tasks.push(task)
-    }
+    add (task: Task) {
+        const missingProperties = ['title', 'description', 'targetDate'].filter(
+          (prop) => !Object.keys(task).includes(prop)
+        )
+        try {
+          if (missingProperties.length > 0) {
+            return 'Missing properties in task object'
+          }
+          this.tasks.push(task)
+        } catch (error) {
+          return error
+        }
+      }
+    
     getTasks(){
         return this.tasks;
     }
