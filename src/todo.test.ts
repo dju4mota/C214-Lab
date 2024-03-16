04d371d6c96c8eafe204de9a128645e2d43fca48
dju4mota <fernandomotajunior2@gmail.com>
2024-03-16 20:43:01 -0300

    Adição dos testes para update e remove

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -28,5 +28,37 @@ describe('ToDoList', () => {
       const tasks = todoInstance.getTasks()
       expect(tasks).toEqual([])
     })
+
+    test('should update the title of a task', () => {
+        const todoInstance = new ToDoList()
+        todoInstance.add(anyTask)
+
+        todoInstance.updateTask(0,{title: 'title_changed',})
+        
+        const tasks = todoInstance.getTasks()
+        expect(tasks[0].title).toEqual('title_changed')
+      })
+
+      test('should update the targetDate of a task', () => {
+        const todoInstance = new ToDoList()
+        todoInstance.add(anyTask)
+
+        todoInstance.updateTask(0,{targetDate: '03/05/2025',})
+        
+        const tasks = todoInstance.getTasks()
+        expect(tasks[0].targetDate).toEqual('03/05/2025')
+      })
+
+      test('should remove a task', () => {
+        const todoInstance = new ToDoList()
+        todoInstance.add(anyTask)
+        
+        todoInstance.removeTask(0)
+
+        const tasks = todoInstance.getTasks()
+        expect(tasks).toEqual([])
+      })
+      
+
   })
 })
